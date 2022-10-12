0444a1af26883757fcae1595374c921c6f13fc48
Taby <forwardpivot@proton.me>
2022-10-12 16:36:00 -0500

    Got the basic functionality. Now i need to change the direction the Y axis moves in

---
+++ script.js
@@ -0,0 +1,27 @@
+const sun = document.querySelector('#sun');
+const ctx = sun.getContext('2d');
+sun.width = window.innerWidth;
+sun.height = window.innerHeight;
+
+let xAxis = 0
+let YAxis = 0
+
+function draw() {
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'orange';
+    
+    ctx.beginPath();
+    ctx.arc(sun.width / 2,YAxis, 25, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+}
+
+function animate() {
+    ctx.clearRect(0,0,sun.width,sun.height);
+
+    YAxis += 1;
+    draw();
+    requestAnimationFrame(animate);
+}
+animate();
